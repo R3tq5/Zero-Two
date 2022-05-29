70ff2a00adf529a4836d0d3fa476b785d5f9a106
R3tq5 <99133787+R3tq5@users.noreply.github.com>
2022-05-29 18:59:18 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -1,93 +1,334 @@
+
 import MessageHandler from '../../Handlers/MessageHandler'
+
 import BaseCommand from '../../lib/BaseCommand'
+
 import WAClient from '../../lib/WAClient'
+
 import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
+
 import { MessageType, Mimetype } from '@adiwajshing/baileys'
+
 import request from '../../lib/request'
 
-
 export default class Command extends BaseCommand {
+
     constructor(client: WAClient, handler: MessageHandler) {
+
         super(client, handler, {
+
             command: 'help',
+
             description: 'Displays the help menu or shows the info of the command provided',
+
             category: 'general',
+
             usage: `${client.config.prefix}help (command_name)`,
-            aliases: ['h']
+
+            aliases: ['h', 'menu']
+
         })
+
     }
 
-    run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
-            const n = [
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+
+        const n = [
+
             'https://c.tenor.com/dEYkyvVk4OQAAAPo/lol.mp4'
-        ]
-        let chitoge = n[Math.floor(Math.random() * n.length)]
-	if (!parsedArgs.joined) {
-			const commands = this.handler.commands.keys();
-			const categories: { [key: string]: ICommand[] } = {};
-			for (const command of commands) {
-				const info = this.handler.commands.get(command);
-				if (!command) continue;
-				if (!info?.config?.category || info.config.category === "dev") continue;
-				if (
-					!info?.config?.category ||
-					(info.config.category === "nsfw" &&
-						!(await this.client.getGroupData(M.from)).nsfw)
-				)
-					continue;
-				if (Object.keys(categories).includes(info.config.category))
-					categories[info.config.category].push(info);
-				else {
-					categories[info.config.category] = [];
-					categories[info.config.category].push(info);
-				}
-			}
-            let text = `
-╭──────────────────𐏋
-│⋊ 𝕌𝕤𝕖𝕣: *${M.sender.username}*
-│⋊ ℕ𝕒𝕞𝕖: Zero Two
-│⋊ ℙ𝕣𝕖𝕗𝕚𝕩: ${this.client.config.prefix}
-│⋊ 𝕆𝕨𝕟𝕖𝕣: *${this.client.config.prefix}mod*
-│⋊ 𝕆𝕗𝕗𝕚𝕥𝕚𝕒𝕝 𝕊𝕦𝕡𝕡𝕠𝕣𝕥 𝔾𝕣𝕠𝕦𝕡: http://gg.gg/10xtwj
-╰──────────────────𐏋                            \n\n`
-            const keys = Object.keys(categories)
-            for (const key of keys)
-                text += `*『 ${this.client.util.capitalize(
-					key
-	         )} 』*\n❐ \`\`\`${categories[key]
-                    .map((command) => command.config?.command)
-                    .join(', ')}\`\`\`\n\n`
-            return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {quoted:M.WAMessage,
-            mimetype: Mimetype.gif,
-            caption: `${text} 
- 
- 
- ──❅┈[ 𝑍𝑒𝑟𝑜 𝑇𝑤𝑜 𝐵𝑜𝑡 ]┈❅──
- 
- 
-┌────────────┈❅
-│   『 Zero Two 』
-│   Based on: Chitoge and Void
-│   Reverse Engineer: Fantox
-└────────────┈⁂
 
-🎗 *Note: Use ${this.client.config.prefix}help <command_name> to view the command info and don't spam or call Zero Two.*` }
-            )
-        }
-        const key = parsedArgs.joined.toLowerCase()
-        const command = this.handler.commands.get(key) || this.handler.aliases.get(key)
-        if (!command) return void M.reply(`No Command of Alias Found | "${key}"`)
-        const state = await this.client.DB.disabledcommands.findOne({ command: command.config.command })
-        M.reply(
-            `🎈 *Command:* ${this.client.util.capitalize(command.config?.command)}\n📉 *Status:* ${
-                state ? 'Disabled' : 'Available'
-            }\n⛩ *Category:* ${this.client.util.capitalize(command.config?.category || '')}${
-                command.config.aliases
-                    ? `\n♦️ *Aliases:* ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
-                    : ''
-            }\n🎐 *Group Only:* ${this.client.util.capitalize(
-                JSON.stringify(!command.config.dm ?? true)
-            )}\n💎 *Usage:* ${command.config?.usage || ''}\n\n📒 *Description:* ${command.config?.description || ''}`
+        ]
+
+        let zerotwo = n[Math.floor(Math.random() * n.length)]
+
+        return void this.client.sendMessage(M.from, { url: zerotwo }, MessageType.video, {quoted:M.WAMessage,
+
+            mimetype: Mimetype.gif,
+
+            caption:`
+
+╭──────────────────𐏋
+
+│⋊ 𝕌𝕤𝕖𝕣: *${M.sender.username}*
+
+│⋊ ℕ𝕒𝕞𝕖: Zero Two
+
+│⋊ ℙ𝕣𝕖𝕗𝕚𝕩: ${this.client.config.prefix}
+
+│⋊ 𝕆𝕨𝕟𝕖𝕣: *${this.client.config.prefix}mod*
+
+│⋊ 𝕆𝕗𝕗𝕚𝕥𝕚𝕒𝕝 𝕊𝕦𝕡𝕡𝕠𝕣𝕥 𝔾𝕣𝕠𝕦𝕡: http://gg.gg/10xtwj
+
+╰──────────────────𐏋  
+
+╭──────────────────𐏋         
+
+│
+
+│	🍁 *Coding* 🍁│- github
+
+│
+
+│	🍁 *Educative Tools* 🍁
+
+│- brainly
+
+│- calculator
+
+│- crypto
+
+│- element
+
+│- ip
+
+│- weather
+
+│- trivia
+
+│- urbandictionary
+
+│
+
+│	🍁 *For Fun* 🍁
+
+│- ppcouple
+
+│- fact
+
+│- jail
+
+│- joke
+
+│- quote
+
+│- rip
+
+│- react
+
+│- ship
+
+│- trash
+
+│- trigger
+
+│- baka
+
+│- wanted
+
+│- why
+
+│- dare
+
+│- truth
+
+│- wasted
+
+│- chat
+
+│- doge
+
+│
+
+│	🍁 *Games* 🍁
+
+│- chess
+
+│
+
+│	🍁 *General Commands* 🍁
+
+│- admins
+
+│- exp
+
+│- help
+
+│- hi
+
+│- info
+
+│- invitelink
+
+│- mods
+
+│- profile
+
+│- rank
+
+│- support
+
+│- zerotwo
+
+│
+
+│	🍁 *Media* 🍁
+
+│- karaoke
+
+│- lyrics
+
+│- play
+
+│- spotify
+
+│- ytaudio
+
+│- ytvideo
+
+│- ytsearch
+
+│- iguser
+
+│- igdl
+
+│- tiktok
+
+│- video
+
+│- pinterest
+
+│- toimg
+
+│
+
+│	🍁 *Admins Only* 🍁
+
+│- activate
+
+│- deactivate
+
+│- open
+
+│- close
+
+│- promote
+
+│- demote
+
+│- delete
+
+│- everyone
+
+│- groupchange
+
+│- revoke
+
+│- remove
+
+│- purge
+
+│
+
+│	🍁 *Nature* 🍁
+
+│- fox
+
+│
+
+│	🍁 *Utilities* 🍁
+
+│- blur
+
+│- circle
+
+│- gif
+
+│- google
+
+│- retrieve
+
+│- screenshot
+
+│- steal
+
+│- sticker
+
+│- stickersearch
+
+│- subred
+
+│- translate
+
+│- wikipedia
+
+│- amazon
+
+│- shorturl
+
+│
+
+│	🍁 *Weeb* 🍁
+
+│- anime
+
+│- animeme
+
+│- character
+
+│- characterid
+
+│- genshincharacter
+
+│- kitsune
+
+│- loli
+
+│- waifu
+
+│- crossplay
+
+│- neko
+
+│- manga
+
+│- pokemon
+
+│- sauce
+
+│- vtuber
+
+│- megumin
+
+│- waifu
+
+│- wallpaper
+
+│- randomwallpaper
+
+╰──────────────────𐏋
+
+╭──────────────────𐏋
+
+│   	 『 Zero Two 』
+
+│   Based on: Chitoge and Void
+
+│   ©️ Synthesized Infinity
+
+╰──────────────────𐏋
+
+`}
+
         )
+
     }
+
 }
+       
+
+
+
+
+
+
+
+
+       
+                 
+                
+         
+                
+          
+        
+    
+
